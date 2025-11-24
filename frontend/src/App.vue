049c2c7b861cc1554a428631e323b3693b2e321c
Heinrich Boers <mail@heinrich-boers.net>
2025-11-24 17:47:48 +0100

    Bootstrap Layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,62 +1,61 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="btn-group">
+    
+    <router-link class="navbar-brand" to="/">TodoApp</router-link>
+    
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarSupportedContent"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+      
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <template v-if="user">
         <li class="nav-item">
           <router-link class="nav-link" to="/">Dashboard</router-link> 
-        </li>
-        <li class="nav-item" v-if="user">
-          <router-link class="nav-link" to="/login">Login</router-link> 
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/register">Register</router-link> 
         </li>
         <li class="nav-item">
           <router-link class="nav-link" to="/todos">Todos</router-link> 
         </li>
+        </template>
+        <template v-else>
+        <li class="nav-item" v-if="user">
+          <router-link class="nav-link" to="/login">Login</router-link> 
+        </li>
+        </template>
       </ul>
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <template v-if="!user">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/login">Login</router-link>
+          </li>
 
-          <!-- Not logged in -->
-          <template v-if="!user">
-            <li class="nav-item">
-              <router-link class="nav-link" to="/login">Login</router-link>
-            </li>
-
-            <li class="nav-item">
-              <router-link class="nav-link" to="/register">Register</router-link>
-            </li>
-          </template>
-
-          <!-- Logged in -->
-          <template v-else>
-            <li class="nav-item">
-              <span class="navbar-text me-3">
-                <i class="bi bi-person-circle"></i>
-                {{ user.email }}
-              </span>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/logout">Logout</router-link>
-            </li>
-          </template>
-        </ul>
-    </div>
-  <span class="float-end" v-if="user">
-          {{ user.email }}
-        </span>
-        <span v-else>
-          Not logged in
-  </span>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/register">Register</router-link>
+          </li>
+        </template>
+        <template v-else>
+          <li class="nav-item">
+            <span class="nav-link disabled">
+              <i class="bi bi-person-circle"></i>
+              {{ user.email }}
+            </span>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/logout">Logout</router-link>
+          </li>
+        </template>
+      </ul>
   </nav>
-
-    
-  <router-view />
+  <div class="container mt-4">
+    <router-view />
+  </div>
 </template>
 <script setup>
-import { computed } from "vue";
-import { currentUser } from "../stores/user";
-
-const user = computed(() => currentUser.value);
-
+  import { computed } from "vue";
+  import { currentUser } from "../stores/user";
+  const user = computed(() => currentUser.value);
 </script>
