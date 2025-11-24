bc053daf875dd7d2dfa169bbaaf7ee77fc763b23
Heinrich Boers <mail@heinrich-boers.net>
2025-11-24 17:12:17 +0100

    added bootstrap

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,48 @@
 <template>
-  <nav>
-  <router-link to="/">Dashboard</router-link> |
-  <router-link to="/login">Login</router-link> |
-  <router-link to="/register">Register</router-link> |
-  <router-link to="/todos">Todos</router-link> |
-  <span v-if="user">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="btn-group">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/">Dashboard</router-link> 
+        </li>
+        <li class="nav-item" v-if="user">
+          <router-link class="nav-link" to="/login">Login</router-link> 
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/register">Register</router-link> 
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/todos">Todos</router-link> 
+        </li>
+      </ul>
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+
+          <!-- Not logged in -->
+          <template v-if="!user">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/login">Login</router-link>
+            </li>
+
+            <li class="nav-item">
+              <router-link class="nav-link" to="/register">Register</router-link>
+            </li>
+          </template>
+
+          <!-- Logged in -->
+          <template v-else>
+            <li class="nav-item">
+              <span class="navbar-text me-3">
+                <i class="bi bi-person-circle"></i>
+                {{ user.email }}
+              </span>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/logout">Logout</router-link>
+            </li>
+          </template>
+        </ul>
+    </div>
+  <span class="float-end" v-if="user">
           {{ user.email }}
         </span>
         <span v-else>
